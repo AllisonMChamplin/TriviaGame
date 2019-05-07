a700e9ca9849e821767c651cbba3a1f61fba3845
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-05-07 12:23:30 -0500

    Updated question display with timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,39 +5,77 @@ $(document).ready(function () {
     var incorrectAnswers = 0;
     var unAnswered = 0;
 
+    var quest = "";
+    var answer = "";
+
+    // Empty array to hold trivia question objects
     var myQuestions = [];
+    // Variable to hold which question is active    
+    var currentQuestion = "";
 
     // Question object constructor
-    function Question(query, opt1, opt2, opt3, opt4) {
+    function Question(query, answerArray) {
         this.query = query;
-        this.optionOne = opt1;
-        this.optionTwo = opt2;
-        this.optionThree = opt3;
-        this.optionFour = opt4;
+        this.answers = answerArray;
         myQuestions.push(this);
     }
 
     // Question objects
-    var questionOne = new Question("Who was absent at the Council of Elrond?", "Gimli", "Legolas", "Galadriel", "Gandalf");
-    var questionTwo = new Question("Who made the One Ring?", "Bilbo", "Saruman", "Elrond", "Sauron");
-    var questionThree = new Question("By what name do the Elves call Gandalf?", "The Grey Pilgrim", "Gandalf the Grey", "Incanus", "Mithrandir");
-    var questionFour = new Question("Who is the proprietor of the Prancing Pony?", "Bill Ferny", "Barliman Butterbur", "Forlong the Fat", "Tom Pickthorn");
+    var questionOne = new Question("Who was absent at the Council of Elrond?", ["Gimli", "Legolas", "Galadriel", "Gandalf"]);
+    var questionTwo = new Question("Who made the One Ring?", ["Bilbo", "Saruman", "Elrond", "Sauron"]);
+    var questionThree = new Question("By what name do the Elves call Gandalf?", ["The Grey Pilgrim", "Gandalf the Grey", "Incanus", "Mithrandir"]);
+    var questionFour = new Question("Who is the proprietor of the Prancing Pony?", ["Bill Ferny", "Barliman Butterbur", "Forlong the Fat", "Tom Pickthorn"]);
 
-    $(loadQuestion);
-    $(loadAnswers);
+    $(init);
 
-    // Replace the question div with a new question
-    function loadQuestion() {
-        var e = myQuestions.shift();
-        $('#question').replaceWith("<p>" + e.query + "</p>");
+    // Start new game
+    function init() {
+        console.log("hi init");
+        $(loadQuestionAll);
+        console.log("myQuestions array: ", myQuestions);
+        console.log("currentQuestion: ", currentQuestion);        
+        console.log("quest: ****", quest);        
+        console.log("answer: ****", answer);
     }
-    // Replace the answer div with the answers
-    function loadAnswers() {
-        $('#answers').replaceWith("<p>" + questionOne.optionOne + "<br>" + questionOne.optionTwo + "<br>" + questionOne.optionThree + "<br>" + questionOne.optionFour + "</p>");
+
+    // Reset game button function
+    $('#reset-button').on("click", function (event) {
+        console.log("reset game!");
+        correctAnswers = 0;
+        incorrectAnswers = 0;
+        unAnswered = 0;
+        myQuestions = [];
+        currentQuestion = "";
+    });
+
+    // Replace the question divs with a new question/answer combo
+    function loadQuestionAll() {
+        console.log("hi loadQuestion");        
+        $('#question').empty();
+        $('#answers').empty();
+        currentQuestion = myQuestions.shift();
+        // Replace the question div with a new question
+        function loadQuestion() {
+            quest = currentQuestion.query;
+            $('#question').html("<p>" + quest + "</p>");
+        };
+
+        // Loops through the array of answers and appends the answer div with each answer array item
+        function loadAnswers() {
+            answer = currentQuestion.answers;
+            for (i = 0; i < answer.length; i++) {
+                $('#answers').append("<li>" + answer[i] + "</li>");
+            };
+        };
+        loadQuestion();
+        loadAnswers();
     }
+
+
+
 
     // Timer stuff
-    var timeLeft = 30;
+    var timeLeft = 3;
     var elem = document.getElementById('timer');
     var timerId = setInterval(countdown, 1000);
 
@@ -52,14 +90,10 @@ $(document).ready(function () {
     }
 
     function doSomething() {
-        console.log("hi"); console.log("hi");
-        $.each(Question, function (index, value) {
-            console.log(index + ': ' + value);
-        });
+        console.log("function doSomething");
+        console.log("currentQuestion: ", currentQuestion);
+        loadQuestionAll();
     }
 
-    console.log("myQuestions: ", myQuestions);
-
-    // Shift questions out
 
 });
