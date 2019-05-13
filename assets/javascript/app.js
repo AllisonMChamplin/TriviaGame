18479f965234e4da1d209fe5509bcd10c8d5f49f
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-05-12 21:52:35 -0500

    Added quote box for answers, updated show/hide buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,9 @@
 $(document).ready(function () {
 
     $(".progress").hide();
+    $("#answers").hide();
+    $(".card").hide();
+    $('#reset').hide();
 
     // Global Variables //
     var correctAnswers = [];
@@ -13,39 +16,50 @@ $(document).ready(function () {
     var questionIdCounter = 0;
     var gameOver = false;
 
+    
     // QUESTION OBJECTS
     var myQuestions = []; // Empty array to hold trivia question objects    
     var currentQuestion = ""; // Variable to hold which question is active
 
     // Question object constructor
-    function Question(query, answerArray, solutionId, questionId) {
+    function Question(query, answerArray, solutionId, questionImg, questionFact, questionId) {
         this.query = query;
         this.answers = answerArray;
         this.solutionId = solutionId;
         this.questionId = questionIdCounter;
+        this.questionImg = questionImg;
+        this.questionFact = questionFact;
         myQuestions.push(this);
         questionIdCounter = questionIdCounter + 1;
     }
 
     // Question Objects //
-    var questionOne = new Question("Who was absent at the Council of Elrond?", ["Gimli", "Legolas", "Galadriel", "Gandalf"], 2);
-    var questionTwo = new Question("Who made the One Ring?", ["Bilbo", "Saruman", "Elrond", "Sauron"], 3);
-    var questionThree = new Question("By what name do the Elves call Gandalf?", ["The Grey Pilgrim", "Gandalf the Grey", "Incanus", "Mithrandir"], 3);
-    var questionFour = new Question("Who is the proprietor of the Prancing Pony?", ["Bill Ferny", "Barliman Butterbur", "Forlong the Fat", "Tom Pickthorn"], 1);
+    var questionOne = new Question("Who was absent at the Council of Elrond?", ["Gimli", "Legolas", "Galadriel", "Gandalf"], 2, "assets/images/quest1.jpg", "blah");
+    var questionTwo = new Question("Who made the One Ring?", ["Bilbo", "Saruman", "Elrond", "Sauron"], 3, "assets/images/quest2.jpg", "blah 2");
+    var questionThree = new Question("By what name do the Elves call Gandalf?", ["The Grey Pilgrim", "Gandalf the Grey", "Incanus", "Mithrandir"], 3, "assets/images/quest3.jpg", "blah 3");
+    var questionFour = new Question("Who is the proprietor of the Prancing Pony?", ["Bill Ferny", "Barliman Butterbur", "Forlong the Fat", "Tom Pickthorn"], 1, "assets/images/quest4.jpg", "blah 4");
+
+
+
 
     // Loads the current question
     function loadQuestion() {
         console.log("Hi loadQuestion", myQuestions);
-
-        $("#next-button").empty();
-        $("#start").hide();
+        $('.progress').empty();
+        $('.progress-bar').addClass('progress');
+        $('.next').remove();
+        $('.card').show();
+        $('#start').hide();
+        $('#question').show();
+        // $("#next-button").empty();
+        // $('.progress').empty();
         
         if (questionIdCounter == 0) {
             $('#timer').empty();
         }
 
         // Set the current question
-        currentQuestion = myQuestions[questionIdCounter];        
+        currentQuestion = myQuestions[questionIdCounter];
 
         if (typeof currentQuestion === "undefined") {
             gameOver == true;
@@ -53,28 +67,28 @@ $(document).ready(function () {
             return false;
         } else {
             // Clear the display divs
-            $(".changer").empty();
-            $('#question').empty();
+            $('.changer').empty();
             $('#answers').empty();
-
+            $('.card').empty();
             // Replace changer with question number
             function displayQuestionNumber() {
                 var x = (questionIdCounter + 1);
                 var n = myQuestions.length;
                 $('.changer').prepend("<p>Question " + x + " of " + n + "</p>");
             }
-            // Replace the changer div with a new question
+            // Replace the changer div with a new question, card div with image
             function displayQuestion() {
                 if (typeof currentQuestion === "undefined") {
                     processGameOver();
                 } else {
                     $('.changer').append(currentQuestion.query);
+                    $('.card').append("<img src=" + currentQuestion.questionImg + ">");
                 }
             };
             // Loops through the array of answers and appends the answer div with each answer array item
             function displayAnswers() {
                 for (i = 0; i < currentQuestion.answers.length; i++) {
-                    $('#answers').append("<button class='answer-button shadow-sm rounded' value=" + i + ">" + currentQuestion.answers[i] + "</button>" + "<br>");
+                    $('#answers').append("<button class='answer-button shadow-sm rounded' value=" + i + ">" + (i + 1) + ") " + currentQuestion.answers[i] + "</button>" + "<br>");
                 };
             };
 
@@ -94,7 +108,8 @@ $(document).ready(function () {
         stop();
         recordLap();
         nextQuestion();
-
+        $('.progress-bar').empty();
+        
         // Adds a class to the button that was clicked
         $(this).addClass('selectedAnswer');
 
@@ -104,14 +119,17 @@ $(document).ready(function () {
 
         // If user guesses the correct answer
         if (this.value == currentQuestion.solutionId) {
-            console.log("correct answer");
             correctAnswers.push(currentQuestion.questionId);
-            console.log("correctAnswer array: ", correctAnswers);
+            $("button[value='" + currentQuestion.solutionId + "']").addClass('correct');
+            $('.card').append("<span class='fact'>Correct! " + currentQuestion.questionFact + "</span>");
+
             // Incorrect answer
         } else if (this.value !== currentQuestion.solutionId) {
-            console.log("incorrect answer");
             incorrectAnswers.push(currentQuestion.questionId);
-            console.log("incorrectAnswer array: ", incorrectAnswers);
+            console.log("HERE");
+            $('.card').append("<span class='fact'>Incorrect. " + currentQuestion.questionFact + "</span>");
+            $(this).addClass('incorrect');
+            $("button[value='" + currentQuestion.solutionId + "']").addClass('correct');
 
             // Timer ran out
         } else {
@@ -125,10 +143,10 @@ $(document).ready(function () {
     var intervalId;
     // prevents the clock from being sped up unnecessarily
     var clockRunning = false;
-    var time = 10;
+    var time = 11;
 
     function reset() {
-        time = 10;
+        time = 11;
         lap = 1;
         // Change the "display" div to "6"
         $("#display").text("6");
@@ -138,13 +156,15 @@ $(document).ready(function () {
     }
 
     function startTiming() {
-        time = 10;
+        time = 11;
         $("#display").text(time);
         // Use setInterval to start the count here and set the clock to running.
         if (!clockRunning) {
             intervalId = setInterval(count, 1000);
-            clockRunning = true;            
-        $(".progress").show();
+            clockRunning = true;
+            $(".progress").show();
+            $("#answers").show();
+            $(".card").show();
         }
     }
 
@@ -171,7 +191,10 @@ $(document).ready(function () {
         unAnswered.push(currentQuestion.questionId);
         console.log("unAnswered array: ", unAnswered);
         $('.answer-button').attr("disabled", true);
+        $('.answer-button').addClass("incorrect");
+        $("button[value='" + currentQuestion.solutionId + "']").addClass('correct');
         nextQuestion();
+        $('.card').append("<span class='fact'>" + currentQuestion.questionFact + "</span>");
     }
 
     function count() {
@@ -186,54 +209,10 @@ $(document).ready(function () {
         progressBar(time);
     }
 
-    // Reset button
-    $("#reset").on("click", reset);
-    function reset() {
-        stop();        
-        $(".progress").hide();
-        correctAnswers = [];
-        incorrectAnswers = [];
-        unAnswered = [];
-        questionIdCounter = 0;
-        gameOver = false;
-        currentQuestion = myQuestions[0];
-        console.log("currentQuestion: ", currentQuestion);
-        time = 10;
-        $("#display").empty();
-        $('#question').empty();
-        $('#answers').empty();
-        $("#next-button").empty();
-        $(".progress").empty();
-    }
-
-
-    // Main Code // // // // //
-
-    // Start button
-    $("#start").on("click", start);
-    function start() {
-        stop();        
-        $(".progress").hide();
-        $(".changer").empty();
-        correctAnswers = [];
-        incorrectAnswers = [];
-        unAnswered = [];
-        questionIdCounter = 0;
-        gameOver = false;
-        currentQuestion = myQuestions[0];
-        time = 10;
-        $("#display").text(time);
-        $('#question').empty();
-        $('#answers').empty();
-        $("#next-button").empty();
-        $(".progress").empty();
-        loadQuestion();
-    }
-
 
     // Next Question
-    function nextQuestion() {
-        $("#next-button").empty();
+    function nextQuestion() {        
+        // $('.progress-bar').removeClass('progress');
         var nextButton = $("<button>");
         nextButton.addClass("next");
         if (questionIdCounter < 4) {
@@ -252,13 +231,16 @@ $(document).ready(function () {
     // Game Over stuff
     function processGameOver() {
         console.log("game over");
+        $('.progress').hide();
         $('#question').empty();
         $('#answers').empty();
-        $("#next-button").empty();
-        $('#question').append("<div class=gameover>" + "Game Over! Here's how you did:" + "</div>" + "<div class=correct>" + correctAnswers.length + " correct.</div>" + "<div class=incorrect>" + incorrectAnswers.length + " incorrect.</div>" + "<div class=unanswered>" + unAnswered.length + " unanswered.</div>");
-        $('.progress').hide();
-        $("#start").text("Start a new game");
-        $("#start").show();
+        $('.changer').empty();
+        $('.main-row').hide();
+        $('.changer').append("<div class=gameover>" + "Game Over!" + "</div>");
+        $('#question').append("<div>Here's how you did:<br><br>" + correctAnswers.length + " correct.<br><br></div>" + "<div>" + incorrectAnswers.length + " incorrect.<br><br></div>" + "<div class=unanswered>" + unAnswered.length + " unanswered.<br><br></div>" + "<br><br><br>");
+        $('#reset').show();
+        $('.card').empty();
+        $('.card').hide();
     };
 
 
@@ -276,5 +258,56 @@ $(document).ready(function () {
         $('.progress').append(progressDiv);
     }
 
+    
+    // Start button
+    $("#start").on("click", start);
+    function start() {
+        $(".changer").html("Are you a fan of J. R. R. Tolkien?<br>Test your knowledge with this timed trivia game! Good luck!");
+        stop();
+        $(".progress").hide();
+        $(".changer").empty();
+        $(".intro-row").empty();
+        correctAnswers = [];
+        incorrectAnswers = [];
+        unAnswered = [];
+        questionIdCounter = 0;
+        gameOver = false;
+        currentQuestion = myQuestions[0];
+        time = 11;
+        $("#display").text(time);
+        $('#question').empty();
+        $('#answers').empty();
+        $(".progress").empty();
+        loadQuestion();
+    }
+
+
+    // Reset button 
+    $('#reset').on("click", reset);
+    function reset() {
+        console.log("hi reset button");
+        stop();
+        $(".progress").hide();
+        $(".changer").empty();
+        $(".changer").html("Are you a fan of J. R. R. Tolkien?<br>Test your knowledge with this timed trivia game! Good luck!");
+        correctAnswers = [];
+        incorrectAnswers = [];
+        unAnswered = [];
+        questionIdCounter = 0;
+        gameOver = false;
+        currentQuestion = myQuestions[0];
+        time = 11;
+        // $("#display").text(time);
+        $('.intro-row').html("<img src='assets/images/intro.png'>");
+        $('#question').empty();
+        $('#answers').empty();
+        $(".progress").empty();
+        // loadQuestion();
+        $('#reset').hide();
+        $('#start').show();
+        $('.main-row').show();
+        $('#answers').show();
+
+    }
 
 });
